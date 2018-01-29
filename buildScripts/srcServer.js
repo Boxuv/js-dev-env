b1aa9da1d0d737ad37b0d6d9b2fbd2c0b3a2f112
aameerik <alar.ameerikas@itcollege.ee>
2018-01-29 19:53:23 +0200

    Transpiling by Babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,22 @@
 // Express allows us to run the app locally in dev mode
+
+// ES 5:
+/*
 var express = require('express');
 var path = require('path');
 var open = require('open');
 
 var port = 3000;
 var app = express();
+*/
+
+// ES 6:
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,9 @@
 // Chalk is a library that allows to specify a color of the output to the console
-var chalk = require('chalk');
+
+//ES 5:
+//var chalk = require('chalk');
+
+//ES 6:
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting up the app in DEV MODE'));
